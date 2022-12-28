fc435b8998920c23983c267ce91affe0e059c0dc
yaditya <yadityasaputra6@gmail.com>
2022-12-28 10:42:36 +0700

    first commit

---
+++ day1.php
@@ -0,0 +1,52 @@
+<?php
+
+// echo 'Program Ganjil_Genap';
+// $number = 1;
+
+// if ($number % 2 == 0){
+//     echo 'Genap';
+// } else {
+//    echo 'Ganjil';
+// }
+
+
+// PROGRAM HITUNG LUAS SEGITIGA
+// DEKLARASI
+// VARIABLE a DAN b TIPE DATA >>> INT
+// VARIABLE HASIL TIPE DATA >>> FLOAT
+// ALGORITMA
+// NILAI MASUK (a.t)
+// HASIL =
+
+// echo PHP_EOL;
+// echo ('Program Hitung Luas Segitiga'). PHP_EOL;
+
+// $a = 6;
+// $t = 20;
+
+// $hasil = (int)(1/2*$a*$t);
+
+// echo ("luas segitiga = $hasil"). PHP_EOL;
+
+// var_dump($hasil);
+
+/* hitung volume balok
+deklarasi
+var p, l, t
+var hasil string
+algoritma 
+input
+*/
+
+
+echo ('Hitung Luas Balok'). PHP_EOL;;
+
+$p = 82;
+$l = 46;
+$t = 18;
+
+$hasil = (string) ($p*$l*$t);
+
+echo ("hasil Luas Balok = $hasil"). PHP_EOL;
+
+var_dump ($hasil);
